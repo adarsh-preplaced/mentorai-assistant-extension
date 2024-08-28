05d2256dae25ace6cba6c1d77a6aa547bab6811a
adarsh-preplaced <adarsh.tiwari@preplaced.in>
2024-08-28 11:59:51 +0530

    fix: z-index positioning

--- src/pages/main/main.tsx
+++ src/pages/main/main.tsx
@@ -7,11 +7,11 @@ const Main = () => {
   const { showPopup, handleTogglePopup } = useMain();
 
   return (
-    <>
+    <div className="z-10000">
       <img
         src="https://firebasestorage.googleapis.com/v0/b/preplaced-upload-dev/o/raj_mentorai.svg?alt=media&token=ba4ad093-a354-4c6c-9649-8488ed6d0061"
         alt=""
-        className="hover:scale-110 transition-all duration-500 ease-in-out absolute bottom-[80px] right-[60px] cursor-pointer"
+        className="hover:scale-110 transition-all duration-500 ease-in-out fixed bottom-[40px] right-[60px] cursor-pointer z-[10000]"
         onClick={handleTogglePopup}
         height={50}
         width={50}
@@ -23,13 +23,13 @@ const Main = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             exit={{ opacity: 0 }}
-            className="w-[500px] h-[80vh] absolute bottom-[120px] right-[100px]"
+            className="w-[500px] h-[80vh] fixed bottom-[80px] right-[100px] z-[10000]"
           >
             <MessageContainer />
           </motion.div>
         )}
       </AnimatePresence>
-    </>
+    </div>
   );
 };
 
